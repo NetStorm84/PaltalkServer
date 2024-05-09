0b8ce46ff86ba00b8fa892f0ac5f3479e8635aa2
Daniel Crawley <netstorm1984@gmail.com>
2024-05-09 13:36:08 +0100

    Updates User schema

--- userModel.js
+++ userModel.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
     uid: { type: Number, required: true, unique: true },
     nickname: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     premium: { type: Boolean, required: true, default: false },
-    admin: { type: Boolean, required: true, default: false }
+    admin: { type: Boolean, required: true, default: false },
+    buddies: [{ type: Schema.Types.ObjectId, ref: 'User' }] // Storing references using _id
 });
 
 const User = mongoose.model('User', userSchema);
